dca60474e4a4bacc332f55d388dbe164580ff90a
b.rense@gmail.com <b.rense@gmail.com@67c98df4-35f9-1bc8-9383-be37a4c8f864>
2012-02-20 07:35:08 +0000

    updating test site

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Train</title>
+<title>Test</title>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <style type="text/css">
 * { margin:0; padding:0; }
